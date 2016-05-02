56e72868db8d914de85316c1d812fa8bf408c732
Krasi Stoyanov <krasimir641@gmail.com>
2016-05-02 14:04:51 +0300

    Added folders and part of the first homework for C# 2

---
+++ CSharp-Part-2/00.Arrays/AllocateArray/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace AllocateArray
+{
+    using System;
+
+    // Problem 1. Allocate array
+
+    // Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+    // Print the obtained array on the console.
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] arr = new int[20];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+            }
+
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
---
+++ CSharp-Part-2/00.Arrays/CompareArrays/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CompareArrays
+{
+    using System;
+    using System.Linq;
+
+    // Problem 2. Compare arrays
+
+    // Write a program that reads two integer arrays from the console and compares them element by element.
+
+    public class Program
+    {
+        public static void Main()
+        {
+            int[] firstArr = Console.ReadLine().Split(' ').Select(character => int.Parse(character)).ToArray();
+            int[] secondArr = Console.ReadLine().Split(' ').Select(character => int.Parse(character)).ToArray();
+        }
+    }
+}
